4ecb843a1264f8e1b8c487c293fb4bd4eee2bd2a
Alexandru Mereacre <mereacre@gmail.com>
2023-01-27 15:20:29 +0000

    feat: Added httplib support

--- lib/cpp-httplib.cmake
+++ lib/cpp-httplib.cmake
@@ -2,8 +2,12 @@ if(BUILD_CPPHTTPLIB_LIB AND NOT BUILD_ONLY_DOCS)
   FetchContent_Declare(
     cpp-httplib
     URL https://github.com/yhirose/cpp-httplib/archive/refs/tags/v0.11.4.tar.gz
-    URL_HASH SHA3_256=9aedcbec09b7b3b01c78cc80822c505846d73928a72ae96eb907b1f467eee649
+    URL_HASH SHA3_256=b19ecc19dc73c239f5bf4ab4816cf630390533590745c552204f5d78e6a502bc
     DOWNLOAD_DIR "${EP_DOWNLOAD_DIR}" # if empty string, uses default download dir
   )
   set(HTTPLIB_REQUIRE_OPENSSL ON CACHE BOOL "Enable OpenSSL for the library" FORCE)
+
+  # create static lib using -fPIC, so we can make it into a sharedobject later
+  set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+  FetchContent_MakeAvailable(cpp-httplib)
 endif ()
--- src/brski.cpp
+++ src/brski.cpp
@@ -1,3 +1,5 @@
+#include <httplib.h>
+
 int main(void) {
   return 0;
 }
