037c4147c0a028b60bff4da551ea95bd831d0ca9
Alexandru Mereacre <mereacre@gmail.com>
2023-02-02 12:56:03 +0000

    feat: Added voucher boilerplate

---
+++ src/voucher/voucher.c
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * @author Alexandru Mereacre
+ * @date 2023
+ * @copyright
+ * SPDX-FileCopyrightText: © 2023 Nquiringminds Ltd
+ * SPDX-License-Identifier: MIT
+ * @brief File containing the implementation of the voucher structure.
+ */
+
+#include "voucher.h"
+
+int test(void) {
+  return 0;
+}
---
+++ src/voucher/voucher.h
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * @author Alexandru Mereacre
+ * @date 2023
+ * @copyright
+ * SPDX-FileCopyrightText: © 2023 Nquiringminds Ltd
+ * SPDX-License-Identifier: MIT
+ * @brief File containing the definition of the voucher structure.
+ */
+#ifndef VOUCHER_H
+#define VOUCHER_H
+#endif
