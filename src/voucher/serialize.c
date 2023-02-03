43a7280e3f5434861d8641d79ca12d025728a339
Alexandru Mereacre <mereacre@gmail.com>
2023-02-03 14:05:32 +0000

    feat: Added the base64 serialization functions

---
+++ src/voucher/serialize.c
@@ -0,0 +1,155 @@
+/**
+ * @file
+ * @author Alexandru Mereacre
+ * @date 2023
+ * @copyright
+ * SPDX-FileCopyrightText: Copyright (c) 2005, Jouni Malinen <j@w1.fi>, 2023 Nquiringminds Ltd
+ * SPDX-License-Identifier: MIT
+ * @brief File containing the definition of the serialization utilities.
+ */
+#include <stdint.h>
+
+#include "../utils/os.h"
+
+static const uint8_t base64_table[65] =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+static uint8_t *base64_gen_encode(const uint8_t *src, size_t len,
+                                        size_t *out_len,
+                                        const uint8_t *table,
+                                        int add_pad) {
+  uint8_t *out, *pos;
+  const uint8_t *end, *in;
+  size_t olen;
+  int line_len;
+
+  if (len >= SIZE_MAX / 4)
+    return NULL;
+  olen = len * 4 / 3 + 4; /* 3-byte blocks to 4-byte */
+  if (add_pad)
+    olen += olen / 72; /* line feeds */
+  olen++;              /* nul termination */
+  if (olen < len)
+    return NULL; /* integer overflow */
+  out = (uint8_t *)sys_malloc(olen);
+  if (out == NULL)
+    return NULL;
+
+  end = src + len;
+  in = src;
+  pos = out;
+  line_len = 0;
+  while (end - in >= 3) {
+    *pos++ = table[(in[0] >> 2) & 0x3f];
+    *pos++ = table[(((in[0] & 0x03) << 4) | (in[1] >> 4)) & 0x3f];
+    *pos++ = table[(((in[1] & 0x0f) << 2) | (in[2] >> 6)) & 0x3f];
+    *pos++ = table[in[2] & 0x3f];
+    in += 3;
+    line_len += 4;
+    if (add_pad && line_len >= 72) {
+      *pos++ = '\n';
+      line_len = 0;
+    }
+  }
+
+  if (end - in) {
+    *pos++ = table[(in[0] >> 2) & 0x3f];
+    if (end - in == 1) {
+      *pos++ = table[((in[0] & 0x03) << 4) & 0x3f];
+      if (add_pad)
+        *pos++ = '=';
+    } else {
+      *pos++ = table[(((in[0] & 0x03) << 4) | (in[1] >> 4)) & 0x3f];
+      *pos++ = table[((in[1] & 0x0f) << 2) & 0x3f];
+    }
+    if (add_pad)
+      *pos++ = '=';
+    line_len += 4;
+  }
+
+  if (add_pad && line_len)
+    *pos++ = '\n';
+
+  *pos = '\0';
+  if (out_len)
+    *out_len = pos - out;
+  return out;
+}
+
+static uint8_t *base64_gen_decode(const uint8_t *src, size_t len,
+                                        size_t *out_len,
+                                        const uint8_t *table) {
+  uint8_t dtable[256], *out, *pos, block[4], tmp;
+  size_t i, count, olen;
+  int pad = 0;
+  size_t extra_pad;
+
+  sys_memset(dtable, 0x80, 256);
+  for (i = 0; i < sizeof(base64_table) - 1; i++)
+    dtable[table[i]] = (uint8_t)i;
+  dtable['='] = 0;
+
+  count = 0;
+  for (i = 0; i < len; i++) {
+    if (dtable[src[i]] != 0x80)
+      count++;
+  }
+
+  if (count == 0)
+    return NULL;
+  extra_pad = (4 - count % 4) % 4;
+
+  olen = (count + extra_pad) / 4 * 3;
+  pos = out = (uint8_t *)sys_malloc(olen);
+  if (out == NULL)
+    return NULL;
+
+  count = 0;
+  for (i = 0; i < len + extra_pad; i++) {
+    uint8_t val;
+
+    if (i >= len)
+      val = '=';
+    else
+      val = src[i];
+    tmp = dtable[val];
+    if (tmp == 0x80)
+      continue;
+
+    if (val == '=')
+      pad++;
+    block[count] = tmp;
+    count++;
+    if (count == 4) {
+      *pos++ = (block[0] << 2) | (block[1] >> 4);
+      *pos++ = (block[1] << 4) | (block[2] >> 2);
+      *pos++ = (block[2] << 6) | block[3];
+      count = 0;
+      if (pad) {
+        if (pad == 1)
+          pos--;
+        else if (pad == 2)
+          pos -= 2;
+        else {
+          /* Invalid padding */
+          sys_free(out);
+          return NULL;
+        }
+        break;
+      }
+    }
+  }
+
+  *out_len = pos - out;
+  return out;
+}
+
+uint8_t *serialize_to_base64(const uint8_t *src, size_t len,
+                             size_t *out_len) {
+  return base64_gen_encode(src, len, out_len, base64_table, 0);
+}
+
+uint8_t *deserialize_from_base64(const uint8_t *src, size_t length,
+                             size_t *out_length) {
+  return base64_gen_decode(src, length, out_length, base64_table);
+}
---
+++ src/voucher/serialize.h
@@ -0,0 +1,45 @@
+/**
+ * @file
+ * @author Alexandru Mereacre
+ * @date 2023
+ * @copyright
+ * SPDX-FileCopyrightText: Copyright (c) 2005, Jouni Malinen <j@w1.fi>, 2023 Nquiringminds Ltd
+ * SPDX-License-Identifier: MIT
+ * @brief File containing the definition of the serialization utilities.
+ */
+#ifndef SERIALIZE_H
+#define SERIALIZE_H
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * @brief Encodes an array to base64
+ *
+ * Caller is responsible for freeing the returned buffer. Returned buffer is
+ * null terminated to make it easier to use as a C string. The null terminator is
+ * not included in out_length.
+ * 
+ * @param[in] src The array to encode
+ * @param[in] length The length of the array to encode
+ * @param[out] out_length The length of the encoded buffer
+ * @return uint8_t * encoded buffer, NULL on failure
+ */
+uint8_t *serialize_to_base64(const uint8_t *src, size_t length,
+                             size_t *out_len);
+
+/**
+ * @brief Decodes a base64 encoded array
+ *
+ * Caller is responsible for freeing the returned buffer.
+ * 
+ * @param[in] src The base64 encoded array
+ * @param[in] length The length of the base64 encoded array
+ * @param[out] out_length The length of the decoded buffer
+ * @return uint8_t * decoded buffer, NULL on failure
+ */
+uint8_t *deserialize_from_base64(const uint8_t *src, size_t length,
+                             size_t *out_length);
+#endif
