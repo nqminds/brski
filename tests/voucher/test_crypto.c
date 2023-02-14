279e031a5b87a03cd37e544145f86fdd30867695
Alexandru Mereacre <mereacre@gmail.com>
2023-02-14 16:28:46 +0000

    tests: wip test crypto eckey to context

--- tests/voucher/test_crypto.c
+++ tests/voucher/test_crypto.c
@@ -19,11 +19,44 @@
 #include "utils/log.h"
 #include "utils/os.h"
 
-#ifdef WITH_OPENSSL
+#ifdef WITH_CRYPTO_OPENSSL
 #include "voucher/crypto_openssl.h"
-#elif WITH_WOLFSSL
+#elif WITH_CRYPTO_WOLFSSL
 #include "voucher/crypto_wolfssl.h"
 #endif
+
+#include "voucher/crypto_defs.h"
+
+static void test_crypto_generate_rsakey(void **state) {
+  (void)state;
+  uint8_t *key = NULL;
+  ssize_t length = crypto_generate_rsakey(2048, &key);
+  assert_non_null(key);
+  assert_true(length > 0);
+  sys_free(key);
+}
+
+static void test_crypto_generate_eckey(void **state) {
+  (void)state;
+  
+  uint8_t *key = NULL;
+  ssize_t length = crypto_generate_eckey(&key);
+  assert_non_null(key);
+  assert_true(length > 0);
+  sys_free(key);
+}
+
+static void test_crypto_eckey2context(void **state) {
+  (void)state;
+
+  uint8_t *key = NULL;
+  ssize_t length = crypto_generate_rsakey(2048, &key);
+
+  CRYPTO_KEY ctx = crypto_eckey2context(key, length);
+  assert_non_null(ctx);
+  crypto_free_keycontext(ctx);
+}
+
 static void test_crypto_free_keycontext(void **state) {
   (void)state;
 }
@@ -36,7 +69,10 @@ int main(int argc, char *argv[]) {
   log_set_quiet(false);
 
   const struct CMUnitTest tests[] = {
-      cmocka_unit_test(test_crypto_free_keycontext),
+    cmocka_unit_test(test_crypto_generate_rsakey),
+    cmocka_unit_test(test_crypto_generate_eckey),
+    cmocka_unit_test(test_crypto_eckey2context),
+    cmocka_unit_test(test_crypto_free_keycontext),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
