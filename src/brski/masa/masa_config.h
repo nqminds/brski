9574830468807dfaafb863e89b5063ce17c5ee62
Alexandru Mereacre <mereacre@gmail.com>
2023-04-04 15:06:23 +0100

    feat: Updated masa/registrar configs

---
+++ src/brski/masa/masa_config.h
@@ -0,0 +1,28 @@
+/**
+ * @file
+ * @author Alexandru Mereacre
+ * @date 2023
+ * @copyright
+ * SPDX-FileCopyrightText: © 2023 Nquiringminds Ltd
+ * SPDX-License-Identifier: MIT
+ * @brief File containing the definition of the masa config options.
+ */
+
+#ifndef MASA_CONFIG_H
+#define MASA_CONFIG_H
+
+#include "../../voucher/array.h"
+
+struct masa_config {
+  char *bind_address;
+  unsigned int port;
+  char *tls_cert_path;
+  char *tls_key_path;
+  char *cms_sign_cert_path;
+  char *cms_sign_key_path;
+  struct BinaryArrayList *cms_add_certs_paths;
+  struct BinaryArrayList *cms_verify_certs_paths;
+  struct BinaryArrayList *cms_verify_store_paths;
+};
+
+#endif
