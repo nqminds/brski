75fb151eba2d40244644d7986d0d663e3cc41a6f
Alexandru Mereacre <mereacre@gmail.com>
2023-04-04 14:16:08 +0100

    featr: Added HTTPS server start

--- src/brski/http/https_server.cpp
+++ src/brski/http/https_server.cpp
@@ -24,8 +24,6 @@ int https_start(struct http_config *config,
                 void **srv_ctx) {
   *srv_ctx = nullptr;
 
-  log_info("Starting the HTTPS server at %s:%d", config->bind_address,
-           config->port);
 #ifdef WITH_CPPHTTPLIB_LIB
   return httplib_start(config, routes, user_ctx, srv_ctx);
 #else
