ffe9896362731f91b742e9bf5c632903cf1452f8
Alexandru Mereacre <mereacre@gmail.com>
2023-04-04 14:56:15 +0100

    feat: Added masa condif

---
+++ src/brski/masa/registrar_config.h
@@ -0,0 +1,24 @@
+/**
+ * @file
+ * @author Alexandru Mereacre
+ * @date 2023
+ * @copyright
+ * SPDX-FileCopyrightText: © 2023 Nquiringminds Ltd
+ * SPDX-License-Identifier: MIT
+ * @brief File containing the definition of the masa config options.
+ */
+
+#ifndef MASA_CONFIG_H
+#define MASA_CONFIG_H
+
+#include "../../voucher/array.h"
+
+struct masa_config {
+  char *bind_address;
+  unsigned int port;
+  char *tls_cert_path;
+  char *tls_key_path;
+  struct BinaryArrayList *additional_cert_paths;
+};
+
+#endif
