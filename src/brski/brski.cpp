nqminds/brski

commit d7d1c07abab4652adbea048cb4f00ceb80fa2abf

feat: Added pinned domain cert output

src/brski/brski.cpp
@@ -227,7 +227,7 @@ int main(int argc, char *argv[]) {
     case COMMAND_PLEDGE_REQUEST:
       fprintf(stdout, "Pledge voucher request to %s:%d\n",
               config.rconf.bind_address, config.rconf.port);
-      if (post_voucher_pledge_request(&config.pconf, &config.rconf, response) < 0) {
+      if (post_voucher_pledge_request(&config.pconf, &config.rconf, &config.mconf, response) < 0) {
         fprintf(stderr, "post_voucher_pledge_request fail");
         return EXIT_FAILURE;
       }

src/brski/pledge/pledge_request.h
@@ -14,6 +14,7 @@
 #include <string>
 
 #include "../registrar/registrar_config.h"
+#include "../masa/masa_config.h"
 #include "pledge_config.h"
 
 /**
@@ -21,10 +22,12 @@
  *
  * @param[in] pconf The pledge configuration structure
  * @param[in] rconf The registrar configuration structure
+ * @param[in] mconf The masa configuration structure
  * @param[out] response The response from the POST request
  * @return int 0 on success, -1 on failure
  */
 int post_voucher_pledge_request(struct pledge_config *pconf,
                                 struct registrar_config *rconf,
+                                struct masa_config *mconf,
                                 std::string &response);
 #endif

src/brski/config.h
@@ -14,6 +14,10 @@
 #include "pledge/pledge_config.h"
 #include "registrar/registrar_config.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /**
  * @brief The BRSKI configuration structures. Used for configuring the
  * server/client/masa
@@ -50,4 +54,9 @@ void free_config_content(struct brski_config *const config);
  */
 int load_cert_files(struct BinaryArrayList *cert_paths,
                     struct BinaryArrayList **out);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif

src/brski/pledge/CMakeLists.txt
@@ -2,4 +2,4 @@ add_library(pledge_utils pledge_utils.c)
 target_link_libraries(pledge_utils PRIVATE array voucher serialize os log)
 
 add_library(pledge_request pledge_request.cpp)
-target_link_libraries(pledge_request PRIVATE pledge_utils https_client serialize os log array voucher)
+target_link_libraries(pledge_request PRIVATE config pledge_utils https_client serialize os log array voucher)

src/brski/pledge/pledge_request.cpp
@@ -17,7 +17,10 @@
 #include "../http/https_client.h"
 #include "../registrar/registrar_config.h"
 #include "../registrar/registrar_api.h"
+#include "../masa/masa_config.h"
 #include "../masa/masa_api.h"
+
+#include "../config.h"
 
 extern "C" {
 #include "../pledge/pledge_utils.h"
@@ -31,6 +34,7 @@ extern "C" {
 
 int post_voucher_pledge_request(struct pledge_config *pconf,
                                 struct registrar_config *rconf,
+                                struct masa_config *mconf,
                                 std::string &response) {
   if (rconf->bind_address == nullptr) {
     log_error("bind_address param is NULL");
@@ -76,6 +80,16 @@ int post_voucher_pledge_request(struct pledge_config *pconf,
 
   const char *masa_pledge_voucher_str = response.c_str();
   struct BinaryArray masa_pledge_voucher_cms = {};
+  struct BinaryArray *nonce = NULL;
+  struct BinaryArray *registrar_tls_cert = NULL;
+  struct BinaryArray *registrar_tls_ca_cert = NULL;
+  struct BinaryArrayList *domain_store = init_array_list();
+  struct BinaryArrayList *masa_verify_certs = NULL;
+  struct BinaryArrayList *masa_store_certs = NULL;
+  struct BinaryArray pinned_domain_cert = {};
+  char *pinned_domain_cert_base64 = NULL;
+
+  int result;
 
   if ((masa_pledge_voucher_cms.length =
            serialize_base64str2array((const uint8_t *)masa_pledge_voucher_str,
@@ -85,10 +99,94 @@ int post_voucher_pledge_request(struct pledge_config *pconf,
     goto post_voucher_pledge_request_fail;
   }
 
+  if (pconf->nonce != NULL) {
+    if ((nonce = (struct BinaryArray *)sys_zalloc(
+             sizeof(struct BinaryArray))) == NULL) {
+      log_errno("sys_zalloc");
+      goto post_voucher_pledge_request_fail;
+    }
+    ssize_t length;
+    if ((length = serialize_base64str2array((const uint8_t *)pconf->nonce,
+                                            strlen(pconf->nonce),
+                                            &nonce->array)) < 0) {
+      log_errno("serialize_base64str2array fail");
+      free_binary_array(nonce);
+      goto post_voucher_pledge_request_fail;
+    }
+    nonce->length = length;
+  }
+
+  if ((registrar_tls_cert = file_to_x509buf(rconf->tls_cert_path)) == NULL) {
+    log_error("file_to_x509buf fail");
+    goto post_voucher_pledge_request_fail;
+  }
+
+  if ((registrar_tls_ca_cert = file_to_x509buf(rconf->tls_ca_cert_path)) == NULL) {
+    log_error("file_to_x509buf fail");
+    goto post_voucher_pledge_request_fail;
+  }
+
+  if(push_array_list(domain_store, registrar_tls_ca_cert->array,
+                    registrar_tls_ca_cert->length, 0) < 0) {
+    log_error("push_array_list");
+    goto post_voucher_pledge_request_fail;
+  }
+
+  if (load_cert_files(pconf->cms_verify_certs_paths, &masa_verify_certs) <
+      0) {
+    log_error("load_cert_files");
+    goto post_voucher_pledge_request_fail;
+  }
+
+  if (load_cert_files(pconf->cms_verify_store_paths, &masa_store_certs) < 0) {
+    log_error("load_cert_files");
+    goto post_voucher_pledge_request_fail;
+  }
+
+  result = verify_masa_pledge_voucher(
+    &masa_pledge_voucher_cms,
+    pconf->serial_number, nonce,
+    registrar_tls_cert,
+    domain_store,
+    masa_verify_certs,
+    masa_store_certs,
+    NULL,
+    &pinned_domain_cert);
+
+  if (result < 0) {
+    log_error("verify_masa_pledge_voucher fail");
+    goto post_voucher_pledge_request_fail;
+  }
+
+  if (serialize_array2base64str(pinned_domain_cert.array,
+                                pinned_domain_cert.length,
+                                (uint8_t **)&pinned_domain_cert_base64) < 0) {
+    log_error("serialize_array2base64str fail");
+    goto post_voucher_pledge_request_fail;
+  }
+
+  response.assign(pinned_domain_cert_base64);
+
   free_binary_array_content(&masa_pledge_voucher_cms);
+  free_binary_array(nonce);
+  free_binary_array(registrar_tls_cert);
+  free_binary_array(registrar_tls_ca_cert);
+  free_array_list(domain_store);
+  free_array_list(masa_verify_certs);
+  free_array_list(masa_store_certs);
+  free_binary_array_content(&pinned_domain_cert);
+  free(pinned_domain_cert_base64);
   return 0;
 
 post_voucher_pledge_request_fail:
   free_binary_array_content(&masa_pledge_voucher_cms);
+  free_binary_array(nonce);
+  free_binary_array(registrar_tls_cert);
+  free_binary_array(registrar_tls_ca_cert);
+  free_array_list(domain_store);
+  free_array_list(masa_verify_certs);
+  free_array_list(masa_store_certs);
+  free_binary_array_content(&pinned_domain_cert);
+  free(pinned_domain_cert_base64);
   return -1;
 }

commit a4fa87982eb0e3f1e11d6db7f3cbfdcbc7ae826b

fix(brski): update pledge request (`preq`) docs
State that `brski preq` returns the `pinned-domain-cert`. This change
was made in
d7d1c07 (feat: Added pinned domain cert output, 2023-05-16).

Fixes: d7d1c07abab4652adbea048cb4f00ceb80fa2abf

src/brski/brski.cpp
@@ -44,7 +44,8 @@ const std::array<struct command_config, 4> command_list = {{
     {"epvr", CommandId::COMMAND_EXPORT_PVR,
      "\tepvr\t\tExport the pledge voucher request as base64 CMS file"},
     {"preq", CommandId::COMMAND_PLEDGE_REQUEST,
-     "\tpreq\t\tSend a pledge-voucher request to the registrar"},
+     "\tpreq\t\tSend a pledge-voucher request to the registrar and\n"
+     "\t\t\t return the pinned-domain-cert."},
     {"registrar", CommandId::COMMAND_START_REGISTRAR,
      "\tregistrar\tStarts the registrar"},
     {"masa", CommandId::COMMAND_START_MASA, "\tmasa\t\tStarts the MASA"},
@@ -217,7 +218,6 @@ int main(int argc, char *argv[]) {
 
   struct RegistrarContext *rcontext = NULL;
   struct MasaContext *mcontext = NULL;
-  std::string response;
   switch (command_id) {
     case CommandId::COMMAND_EXPORT_PVR:
       std::fprintf(stdout, "Exporting pledge voucher request to %s",
@@ -229,9 +229,10 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
       }
       break;
-    case CommandId::COMMAND_PLEDGE_REQUEST:
+    case CommandId::COMMAND_PLEDGE_REQUEST: {
       std::fprintf(stdout, "Pledge voucher request to %s:%d\n",
                    config.rconf.bind_address, config.rconf.port);
+      std::string response;
       if (post_voucher_pledge_request(&config.pconf, &config.rconf,
                                       &config.mconf, response) < 0) {
         std::fprintf(stderr, "post_voucher_pledge_request fail");
@@ -239,6 +240,7 @@ int main(int argc, char *argv[]) {
       }
       std::fprintf(stdout, "%s\n", response.c_str());
       break;
+    }
     case CommandId::COMMAND_START_REGISTRAR:
       if (registrar_start(&config.rconf, &config.mconf, &config.pconf,
                           &rcontext) < 0) {

src/brski/pledge/pledge_request.h
@@ -23,7 +23,8 @@
  * @param[in] pconf The pledge configuration structure
  * @param[in] rconf The registrar configuration structure
  * @param[in] mconf The masa configuration structure
- * @param[out] response The response from the POST request
+ * @param[out] response The pinned domain certificate in DER format, encoded as
+ * base64.
  * @return int 0 on success, -1 on failure
  */
 int post_voucher_pledge_request(struct pledge_config *pconf,